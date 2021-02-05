621f9fd522060986fd6ff7f1fc16b3daf30269eb
germancin <elmaildegerman@gmail.com>
2021-02-05 17:05:28 -0500

    setup for cv2 32 workers

--- image_thread_pooling.py
+++ image_thread_pooling.py
@@ -18,19 +18,18 @@ def fibo(n):
 
     return total_sum
 
-def wait_function(position):
+def wait_function(exeId):
     start = time.time()
     n = random.randint(99999, 999999)
     total_sum = fibo(n)
-    # time.sleep(random.randint(1, 5))
-    print(f"Fibo de: {n} ::::Execution time: {time.time() - start}   completed {position}")
-    return total_sum
+    print(f"Fibo de: {n} ::::Execution time: {time.time() - start}   completed ({exeId})")
+    # return total_sum
 
 def callback_function(future):
-    # print('Callback with the following result', future.result())
+    print('Callback with the following result', future.result())
     print('Callback with the following result')
 
-with ThreadPoolExecutor(max_workers=32) as executor: #change max_workers to 2 and see the results
+with ThreadPoolExecutor(max_workers=2) as executor: #change max_workers to 2 and see the results
     future = executor.submit(wait_function, 'first')
     future.add_done_callback(callback_function)
     future2 = executor.submit(wait_function, 'second')
