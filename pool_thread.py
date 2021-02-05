fe55f2310f68ec0406c7b7e44373553ff3e8bc78
German Gabriel <germancin@users.noreply.github.com>
2021-02-05 13:18:01 -0500

    print

--- pool_thread.py
+++ pool_thread.py
@@ -17,7 +17,7 @@ def main():
     executor = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
     for i in range(multiprocessing.cpu_count()):
        task_exe = executor.submit(task)
-       # print(task_exe)
+       print(f'Task# {i}')
 
 if __name__ == '__main__':
     main()
