228e6ba33ac3dcc36e4a3947527ccdd8f30a754b
germancin <elmaildegerman@gmail.com>
2021-02-05 18:01:28 +0000

    threading basic example

---
+++ pool_thread.py
@@ -0,0 +1,23 @@
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import random
+import multiprocessing
+
+
+def task():
+    print("Executing our Task")
+    result = 0
+    i = 0
+    for i in range(10):
+        result = result + i
+    print("I: {}".format(result))
+    print("Task Executed {}".format(threading.current_thread()))
+
+def main():
+    executor = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
+    for i in range(multiprocessing.cpu_count()):
+       task_exe = executor.submit(task)
+       print(task_exe)
+
+if __name__ == '__main__':
+    main()
