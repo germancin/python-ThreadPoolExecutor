d88880e23033a2127ce627716082bfc121cd85e7
germancin <elmaildegerman@gmail.com>
2021-02-05 20:38:34 -0500

    setup for cv2 32 workers

--- thread_pool_executor.py
+++ thread_pool_executor.py
@@ -1,39 +1,43 @@
-from concurrent.futures import ProcessPoolExecutor
-#from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 import time
+import os
 import random
 
-def wait_function(x, y):
+def fibo(n):
+    a = 0
+    b = 1
+    total_sum = 0
+    count = 1
+    # print("Fibonacci Series: ", end=" ")
+    while count <= n:
+        # print(total_sum, end=" ")
+        count += 1
+        a = b
+        b = total_sum
+        total_sum = a + b
+
+    return total_sum
+
+def wait_function(exeId):
     start = time.time()
-    time.sleep(random.randint(1, 5))
-    print(f"Task( multiply {x} times(*) {y}, ) Execution time: {time.time() - start} completed")
-    return x * y
+    n = random.randint(99999, 999999)
+    total_sum = fibo(n)
+    print(f"Fibo de: {n} ::::Execution time: {time.time() - start}   completed ({exeId})")
+    # return total_sum
 
 def callback_function(future):
     print('Callback with the following result', future.result())
+    print('Callback with the following result')
 
-with ProcessPoolExecutor(max_workers=2) as executor: #change max_workers to 2 and see the results
-#with ThreadPoolExecutor(max_workers=1) as executor: #change max_workers to 2 and see the results
-
-    future = executor.submit(wait_function, 1, 1)
+with ThreadPoolExecutor(max_workers=2) as executor: #change max_workers to 2 and see the results
+    future = executor.submit(wait_function, 'first')
     future.add_done_callback(callback_function)
-
-    future2 = executor.submit(wait_function, 2, 2)
+    future2 = executor.submit(wait_function, 'second')
     future2.add_done_callback(callback_function)
-
-    future3 = executor.submit(wait_function, 3, 3)
-    future3.add_done_callback(callback_function)
 
     start = time.time()
     while True:
-        # if(future.running()):
-        #     print("Task 1 running")
-        # if(future2.running()):
-        #     print("Task 2 running")
-        # if (future3.running()):
-        #     print("Task 3 running")
-
-        if(future.done() and future2.done() and future3.done()):
-            print(future.result(), future2.result(), future3.result())
+        if(future.done() and future2.done()):
+            # print(future.result(), future2.result())
             break
     print(time.time() - start)
