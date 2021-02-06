055dd505918385a0fec277714d969dfdd6983045
germancin <elmaildegerman@gmail.com>
2021-02-06 15:17:59 -0500

    testing different escenarios and convinations

--- parallel.py
+++ parallel.py
@@ -139,13 +139,14 @@ for im in images_target_path:
 target_imgs_count = len(new_images_target)
 all_images_target = chunks(new_images_target, 10)
 images_target_path = all_images_target
-
+cont = 0
 with ThreadPoolExecutor(max_workers=worker_count) as executor:  # change max_workers to 2 and see the results
     for idx, image_target_chunk in enumerate(images_target_path):
         for index, image_target in enumerate(image_target_chunk):
             globals()[f'future{idx}'] = executor.submit(wait_function, image_target, new_imgs_path, images_subject)
-            # globals()[f'future{idx}'].add_done_callback(callback_function)
-            print(f"GLOBLS { f'future{idx}' } - TTTT {image_target}-({index})")
+            # globals()[f'future{idx}'].add_done_callback(callback_function)\
+            cont = cont + 1
+            print(f"GLOBAL{f'future{idx}'} ----- {image_target}-({index}) --- {cont}")
 
     # while True:
     #     if ([f'future{idx}'].done()):
