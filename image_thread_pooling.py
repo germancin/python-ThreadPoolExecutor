cf2aa558055759232ec5648cd5bf1da7f31c87b2
germancin <elmaildegerman@gmail.com>
2021-02-06 13:06:04 -0500

    testing different escenarios and convinations

--- image_thread_pooling.py
+++ image_thread_pooling.py
@@ -117,7 +117,7 @@ def chunks(imgs_target_path, n):
 
  # Init App #
 start = time.time()
-worker_count = 40
+worker_count = 33
 percentage = 0.50
 base_path = os.getcwd()
 img_subjects_path = os.path.join(base_path, "images_subject")
