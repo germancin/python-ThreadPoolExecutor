82de06d8c84c5af1bfc53aea566cb60330097ed1
germancin <elmaildegerman@gmail.com>
2021-02-07 00:21:22 -0500

    testing different escenarios and convinations

--- parallel.py
+++ parallel.py
@@ -132,8 +132,8 @@ start = time.time()
 ################################
 #
 ################################
-worker_count = multiprocessing.cpu_count()
-# worker_count = 37
+# worker_count = multiprocessing.cpu_count()
+worker_count = 32
 percentage = 0.50
 base_path = os.getcwd()
 img_subjects_path = os.path.join(base_path, "images_subject")
