a9159e082874dd3f5545f10320596142117836a2
germancin <elmaildegerman@gmail.com>
2021-02-06 21:45:11 -0500

    testing different escenarios and convinations

--- parallel.py
+++ parallel.py
@@ -139,7 +139,7 @@ for im in images_target_path:
         new_images_target.append(im)
 
 target_imgs_count = len(new_images_target)
-all_images_target = chunks(new_images_target, 1)
+all_images_target = chunks(new_images_target, 3)
 images_target_path = all_images_target
 cont = 0
 with ThreadPoolExecutor(max_workers=worker_count) as executor:  # change max_workers to 2 and see the results
